4cb7ddac1a680d4e5ca13db5f0a3495e6423d3bb
rahul120799 <46495839+rahul120799@users.noreply.github.com>
2019-06-12 20:56:11 +0530

    Create exno46pro.py

---
+++ exno46pro.py
@@ -0,0 +1,13 @@
+o=int(input())
+l=list(map(int,input().split()))
+a_ha=0
+b_ha=0
+l.sort(reverse=True)
+for i in l:
+  s=a_ha+i
+  if b_ha>s:
+    a_ha=s
+  else:
+    a_ha=b_ha
+    b_ha=s
+print(a_ha,b_ha)
