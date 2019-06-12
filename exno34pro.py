eb6db897a712fd8552aed7f910d745f1a3231ad0
rahul120799 <46495839+rahul120799@users.noreply.github.com>
2019-06-12 20:17:07 +0530

    Create exno34pro.py

---
+++ exno34pro.py
@@ -0,0 +1,12 @@
+o,Y1=map(int,input().split())
+D1=0
+Li=[]
+for i in range(o):
+      Li.append(input())
+for i in range(X1):
+      for j in range(Y1-1):
+            if(Li[i][j]!='R' and Li[i][j+1]!='R'):
+                  D1+=3
+            elif(Li[i][j]!='G' and Li[i][j+1]!='G'):
+                  D1+=5
+print(D1)
