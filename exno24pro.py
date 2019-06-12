6a32285f2eeb22a22a3c18720cc3886dc2e8a83f
rahul120799 <46495839+rahul120799@users.noreply.github.com>
2019-06-12 19:44:38 +0530

    Create exno24pro.py

---
+++ exno24pro.py
@@ -0,0 +1,13 @@
+o=int(input())
+a=[]
+k=bin(2**n1-1)[2::]
+l=len(k)
+for i in range(0,2**o):
+	s=bin(i)[2::]
+	if len(s)<l:
+		a.append([s.count("1"),(l-len(s))*"0"+s])
+	else:
+		a.append([s.count("1"),s])
+a.sort()
+for i in range(0,len(a)):
+	print(a[i][1])
