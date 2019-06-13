9ad9b0cb7f947235c2a060170e8151c99ec1277b
rahul120799 <46495839+rahul120799@users.noreply.github.com>
2019-06-13 11:09:10 +0530

    Create exno58pro.py

---
+++ exno58pro.py
@@ -0,0 +1,14 @@
+o= int(input())
+b = []
+a = o//2 + o
+for i in range(1,o+1):
+  if i%2==0:
+    b.append(i)
+for i in range(1,n+1):
+  if i%2!=0:
+    b.append(i)
+for i in range(1,o+1):
+  if i%2==0:
+    b.append(i)
+print(a)
+print(*b)
