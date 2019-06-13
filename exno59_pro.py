613b08c88f870054b9c3e05f18b4aeb5cedca7ac
rahul120799 <46495839+rahul120799@users.noreply.github.com>
2019-06-13 11:18:46 +0530

    Update exno59_pro.py

--- exno59_pro.py
+++ exno59_pro.py
@@ -1,9 +1,9 @@
 o= input()
-b = input()
+u = input()
 c = o.split('|')
-if len(c[0]+b)==len(c[1]):
-  print(c[0]+b+'|'+c[1])
-elif len(c[0])==len(c[1]+b):
-  print(c[0]+'|'+c[1]+b)
+if len(c[0]+u)==len(c[1]):
+  print(c[0]+u+'|'+c[1])
+elif len(c[0])==len(c[1]+u):
+  print(c[0]+'|'+c[1]+u)
 else:
   print("Impossible")
